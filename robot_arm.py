f4b584fbbb5f3369704602a93ab8d5fe1576636b
Porthoos <74708619+Porthoos@users.noreply.github.com>
2021-11-22 21:00:56 +0800

    Update robot_arm.py

--- robot_arm.py
+++ robot_arm.py
@@ -13,24 +13,35 @@ while(1):
    # print(content)
     if len(content)==0:
         continue
-    x = content[0].split(" ")[1]
-    y = content[0].split(" ")[2]
-    z = content[0].split(" ")[3]
+    x = float(content[0].split(" ")[1])
+    y = float(content[0].split(" ")[2])
+    z = float(content[0].split(" ")[3])
+
+    X = int(z) - 300
+    Y = -int(x) + 165
+    Z = int(y) + 120
 
     openfile.close()
     file=open(file_path,'w+')
     file.close()
 
-    print(x)
-    print(y)
-    print(z)
+    if x==0 and y==0 and z==0:
+        continue
+
+    #print(x, y, z)
+    print(X, Y, Z)
 
     # arm.setIK6([int((pr[0][0]+pr[1][0])/2), int((pr[0][1]+pr[1][1])/2), 150], [0, 0, -1])  # move to [x=0,y=200,=150]
     # arm.setIK6([110,250,150],[0,0,-1])
     # time.sleep(1)
     # arm.setIK6([int((pr[0][0]+pr[1][0])/2), int((pr[0][1]+pr[1][1])/2), 30], [0, 0, -1])  # down
 
-    arm.setAngle(6, 90)  # open hand
-    time.sleep(1)
+    arm.setIK6([X, Y, Z], [X, Y, 0])
+    time.sleep(2)
+
     arm.setAngle(6, 18)  # close hand
+    time.sleep(1)
+    arm.setIK6([0, 200, 150], [0, 0, -1])  # move back to [x=0,y=200,=150]
+    time.sleep(1)
+    arm.setAngle(6, 90)  # open hand
     time.sleep(1)
